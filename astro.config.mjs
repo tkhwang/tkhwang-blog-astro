ef95bae4282d5307ec87959a821dad559d7a4c50
tkhwang <tkhwang@gmail.com>
2023-01-10 12:44:48 +0900

    feat(common): :sparkles: update site base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tkhwang.github.io",
+  site: "https://tkhwang.me",
   integrations: [
     tailwind({
       config: {
